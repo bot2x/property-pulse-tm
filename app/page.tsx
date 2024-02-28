52e6bbcf2dc41b132a9655ae7f317487efd77679
Shubham <shubham0921s@gmail.com>
2024-02-28 19:04:35 +0530

    adds home and property pages with json data for now.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import Hero from "@/components/hero";
+import Footer from "@/components/footer";
+import InfoBoxes from "@/components/infoboxes";
+
 
 export default function Home() {
   return (
     <div>
-      This is the home page.
+      <Hero />
+      <InfoBoxes />
+      <Footer />
     </div>
   );
 }
--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,9 +1,30 @@
+import properties from "@/properties.json";
+import PropertyCard from "@/components/propertycard";
 
 
 const PropertiesPage = () => {
+  // console.log(properties);
+  // const properties = [];
   return (
-    <div>PropertiesPage</div>
-  )
-}
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 
+        ? (<div>No properties available.</div>) 
+        : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard 
+                key={property._id}
+                propertyInfo = {property}
+              />
+              // <div>{property.name} </div>
+            ))}
+          </div>
+        )}
+        
+      </div>
+    </section>
+  );
+};
 
-export default PropertiesPage
+export default PropertiesPage;
