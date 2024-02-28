60116da155e6f43715bc826b5883cc4943e45fd2
Shubham <shubham0921s@gmail.com>
2024-02-29 04:08:36 +0530

    adds dbconfig to add mongodb in the project

---
+++ dbconfig/database.js
@@ -0,0 +1,24 @@
+import mongoose, { mongo } from "mongoose";
+
+let connected = false;
+
+const connectDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    //if db already connected. Don't connect again.
+    if (connected) {
+        console.log("Mongodb already connected.");
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        connected = true;
+        console.log("Mongodb connected...");
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export default connectDB;
